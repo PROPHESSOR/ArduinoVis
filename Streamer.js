97499747c549e7413d1eaf733cafa37906b25639
PROPHESSOR <xenteze@gmail.com>
2017-10-03 21:17:34 +0300

    Streamer init

--- Streamer.js
+++ Streamer.js
@@ -1,3 +1,12 @@
-const Streamer = {}
+const cp = require("child_process");
+
+const Streamer = {
+    _init: function() {
+        //TODO: Write me...
+        let pacat = cp.spawn("pacat", ["-r", "--channels=1", "--raw", "--format=s16le", "--rate=44100", "-n", "Arduino"]);
+        pacat.stdout.on('data', Streamer.onData);
+    },
+    onData:data=>0
+}
 
 module.exports = Streamer;
